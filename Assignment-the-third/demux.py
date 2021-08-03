8544353b7ef3af158e43f1b54b6c01d4df349838
James Adler <jamespadler@gmail.com>
2021-08-02 23:59:12 -0700

    test improvements and dynamic file creation

--- Assignment-the-third/demux.py
+++ Assignment-the-third/demux.py
@@ -9,6 +9,7 @@ import gzip
 import numpy as np
 import matplotlib.pyplot as plt
 import io
+from Bioinfo import convert_phred
 
 
 # define arparser
@@ -31,6 +32,7 @@ read1_file: str = str(args.r1)
 read2_file: str = str(args.r2)
 index1_file: str = str(args.i1)
 index2_file: str = str(args.i2)
+barcodes_file: str = str(args.b)
 read_length: int = int(args.l)
 num_reads: int = int(args.n)
 
@@ -39,6 +41,7 @@ assert read1_file.endswith('.fastq.gz'), "Read 1 file must be .fastq.gz"
 assert read2_file.endswith('.fastq.gz'), "Read 2 file must be .fastq.gz"
 assert index1_file.endswith('.fastq.gz'), "Index 1 file must be .fastq.gz"
 assert index2_file.endswith('.fastq.gz'), "Index 2 file must be .fastq.gz"
+assert barcodes_file.endswith('.tsv'), "Barcodes file must be .tsv"
 
 # define reverse function
 def rev_complement(strand):
@@ -64,11 +67,54 @@ def rev_complement(strand):
     
     return rev_comp
 
-# open input files
+# dynamic file opening
+####################################################################################################
+# TODO: need to figure out how to open each of these files;
+    # maybe we can have a list of unique variables that we index
+    # in order to name each of the files, for example, if we
+    # have a list of random character variable names and 26 input files
+    # then we would index 0-25 of our random character variable list
+barcodes = {}
+with open(barcodes_file) as b:
+    for line in b:
+        line = line.rstrip().split('\t')
+        barcodes[line[1]] = [line[0]]
+# barcodes = sorted(barcodes)
+print(barcodes)
+
+
+# generate and open output files
+# TODO: THIS DYNAMICALLY CREATES ALL OF OUR BARCODE FILES
+    # SO CAN WE NOW JUST USE THE ITEMS IN THE LIST TO ACCESS
+    # AND WRITE TO EACH OF THOSE FILES?
+# TODO: SWITCH MY BARCODES DICTIONARY SO WE CAN USE THE 
+    # BARCODE TO ACCESS THE CORRECT POSITION OF OUR LIST
+
+# so here we are dynamically creating our output text files based on the names
+    # of the barcodes that come in. we store the object in the dictionary
+    # which can be accessed using the BARCODE. <- by creating access using
+    # the BARCODE, then we can easily compare to our INDEXES that we find up above
+        
+        # FOR EXAMPLE:
+            # if the index is a match, then write to the file that is associated
+                # with that index
+
+for f in barcodes:
+    file = '{}.txt'.format(barcodes[f][0])
+    barcodes[f].append(open(file, 'w'))
+print(barcodes)
+
+##################################################################################################
+
+# open standard input and output files
 with (gzip.open(read1_file) as rf1,
     gzip.open(read2_file) as rf2,
     gzip.open(index1_file) as if1,
-    gzip.open(index2_file) as if2):
+    gzip.open(index2_file) as if2,
+    open('read1_lowqual.fastq','w') as lowqual1,
+    open('read2_lowqual.fastq','w') as lowqual2,
+    open('read1_mismatch.fastq','w') as mismatch1,
+    open('read2_mismatch.fastq','w') as mismatch2):
 
     # read 1st records from file
     rf1_record = [rf1.readline().decode('utf-8').rstrip() for _ in range(4)]
@@ -85,20 +131,47 @@ with (gzip.open(read1_file) as rf1,
         # reverse complement index 2
         i2_rev = rev_complement(if2_record[1])
 
-        # TODO: check quality score of reads
+        # check quality score reads
+        # TODO: may be a good idea to work a quality calculation check in here
+        rf1_qscore = sum([int(convert_phred(letter)) for letter in rf1_record[3]]) / len(rf1_record[3])
+        rf2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
+        if1_qscore = sum([int(convert_phred(letter)) for letter in if1_record[3]]) / len(if1_record[3])
+        if2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
+
+        # lowest qscore
+        low_qscore = min([rf1_qscore,rf2_qscore,if1_qscore,if2_qscore])
+
+        # generate new header
+        new_header1 = rf1_record[0] + ':' + if1_record[1] + '-' + i2_rev
+        new_header2 = rf2_record[0] + ':' + if1_record[1] + '-' + i2_rev
+        rf1_record[0] = new_header1 # TODO: probably a more creative way to assign the new header?
+        rf2_record[0] = new_header2
 
         # check for unknown
-        if 'N' in if1_record[1] or 'N' in if2_record[1]:
-            print('UNKNOWN!', if1_record[1], i2_rev)
+        if 'N' in if1_record[1] or 'N' in if2_record[1] or low_qscore < 20:
+            print('UNKNOWN OR LOW QUALITY!', if1_record[1], i2_rev)
+            for component in rf1_record:
+                lowqual1.write(component + '\n')
+            for component in rf2_record:
+                lowqual2.write(component + '\n')
         
-        # TODO: check if low quality
-
+        # # check for low quality
+        # elif low_qscore < 20:
+        #     print(new_header)
+        #     print('LOW QUALITY!', if1_record[1], i2_rev)
 
         # check for index match
         elif if1_record[1] == i2_rev:
             print('MATCH!', if1_record[1], i2_rev)
+            print('writing to {}.txt'.format(barcodes[if1_record[1]][0]))
+            for component in if1_record:
+                barcodes[if1_record[1]][1].write(component + '\n')
         else:
             print('MISMATCH!', if1_record[1], i2_rev)
+            for component in rf1_record:
+                mismatch1.write(component + '\n')
+            for component in rf2_record:
+                mismatch2.write(component + '\n')
         
 
         # read next record
@@ -112,19 +185,12 @@ with (gzip.open(read1_file) as rf1,
         if recordcount % 10000000 == 0:
             print('Records processed:', recordcount)
 
+# close each of our dynamic output files
+for f in barcodes:
+    barcodes[f][1].close()
+    barcodes[f].pop(-1)
 
-
-# TODO: need to figure out how to open each of these files;
-    # maybe we can have a list of unique variables that we index
-    # in order to name each of the files, for example, if we
-    # have a list of random character variable names and 26 input files
-    # then we would index 0-25 of our random character variable list
-
-
-# generate and open output files
-
-
-
+print(barcodes)
 
 
 
