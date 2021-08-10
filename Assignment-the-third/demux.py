050ed5be4da1ed185d450f50983eaedde175f6d4
James Adler <jamespadler@gmail.com>
2021-08-10 11:41:58 -0700

    moved open statements outside of with statement

--- Assignment-the-third/demux.py
+++ Assignment-the-third/demux.py
@@ -63,7 +63,7 @@ def rev_complement(strand):
     
     return rev_comp
 
-# dynamically open a read1 and read2 for each barcode
+# establish barcodes dictionary of dictionary (key = index, value = dict(barcode, read1file, read2file, counter))
 barcodes = {}
 with open(barcodes_file) as b:
     for line in b:
@@ -84,102 +84,136 @@ for f in barcodes:
     barcodes[f]['read1file'] = open(read1_match, 'w')
     barcodes[f]['read2file'] = open(read2_match, 'w')
 
-# open standard input and output files
-with gzip.open(read1_file, 'rt') as rf1, \
-    gzip.open(read2_file, 'rt') as rf2, \
-    gzip.open(index1_file, 'rt') as if1, \
-    gzip.open(index2_file, 'rt') as if2, \
-    open('read1_lowqual.fastq','w') as lowqual1, \
-    open('read2_lowqual.fastq','w') as lowqual2, \
-    open('read1_mismatch.fastq','w') as mismatch1, \
-    open('read2_mismatch.fastq','w') as mismatch2:
+# generate swap dictionary and counter for tracking instance of each swap
+swap_counter = 0
+swap_dict = {}
+for i1 in barcodes:
+    for i2 in barcodes:
+        if i1 != i2:
+            index_combo = i1 + '-' + i2
+            swap_dict[index_combo] = 0
+print('length of swap dict:',len(swap_dict))
 
-    # recordcount tracker
-    recordcount = 0
+# open standard input files
+rf1 = gzip.open(read1_file, 'rt')
+rf2 = gzip.open(read2_file, 'rt')
+if1 = gzip.open(index1_file, 'rt')
+if2 = gzip.open(index2_file, 'rt')
 
-    # count index swaps
-    swap_counter = 0
+# open standard output files
+lowqual1 = pen('read1_lowqual.fastq','w')
+lowqual2 = open('read2_lowqual.fastq','w')
+mismatch1 = open('read1_mismatch.fastq','w')
+mismatch2 = open('read2_mismatch.fastq','w')
 
-    # loop and break at end of file
-    while True:
+# recordcount tracker
+recordcount = 0
 
-        # read next record
-        rf1_record = [rf1.readline().rstrip() for _ in range(4)]
-        rf2_record = [rf2.readline().rstrip() for _ in range(4)]
-        if1_record = [if1.readline().rstrip() for _ in range(4)]
-        if2_record = [if2.readline().rstrip() for _ in range(4)]
+# unknown counter
+unknown_counter = 0
 
-        # break at end of file
-        if rf1_record[0] == '':
-            break
+# low quality tracker
+lowqual_counter = 0
 
-        # increment recordcount and update statement
-        recordcount += 1
-        if recordcount % 100 == 0:
-            print('Records processed:', recordcount)
+# loop and break at end of file
+while True:
 
-        # reverse complement index 2
-        i2_rev = rev_complement(if2_record[1])
+    # read next record
+    rf1_record = [rf1.readline().rstrip() for _ in range(4)]
+    rf2_record = [rf2.readline().rstrip() for _ in range(4)]
+    if1_record = [if1.readline().rstrip() for _ in range(4)]
+    if2_record = [if2.readline().rstrip() for _ in range(4)]
 
-        # generate new header
-        new_header1 = rf1_record[0] + ':' + if1_record[1] + '-' + i2_rev
-        new_header2 = rf2_record[0] + ':' + if1_record[1] + '-' + i2_rev
-        rf1_record[0] = new_header1 # TODO: probably a more creative way to assign the new header?
-        rf2_record[0] = new_header2
+    # break at end of file
+    if rf1_record[0] == '':
+        break
 
-        # if contains unknown base in index, write to low qual
-        if 'N' in if1_record[1] or 'N' in if2_record[1]:
-            for component in rf1_record:
-                lowqual1.write(component + '\n')
-            for component in rf2_record:
-                lowqual2.write(component + '\n')
-            continue
+    # increment recordcount and update statement
+    recordcount += 1
+    if recordcount % 100 == 0:
+        print('Records processed:', recordcount)
 
-        # if not valid barcode, write to low qual
-        if if1_record[1] not in barcodes:
-            for component in rf1_record:
-                lowqual1.write(component + '\n')
-            for component in rf2_record:
-                lowqual2.write(component + '\n')
-            continue
+    # reverse complement index 2
+    i2_rev = rev_complement(if2_record[1])
 
-        # check quality score reads
-        # TODO: may be a good idea to work a quality calculation check in here (unittest)
-        # TODO: turn this into a function
-        # TODO: calculate quality scores inline so that we aren't wasting resources calculating all 4 every time (MAYBE THIS IS WHERE I'M LOSING TIME)
-        rf1_qscore = sum([int(convert_phred(letter)) for letter in rf1_record[3]]) / len(rf1_record[3])
-        rf2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
-        if1_qscore = sum([int(convert_phred(letter)) for letter in if1_record[3]]) / len(if1_record[3])
-        if2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
+    # generate new header
+    index_combo = if1_record[1] + '-' + i2_rev
+    new_header1 = rf1_record[0] + ':' + index_combo
+    new_header2 = rf2_record[0] + ':' + index_combo
+    rf1_record[0] = new_header1 # TODO: probably a more creative way to assign the new header?
+    rf2_record[0] = new_header2
 
-        # lowest qscore
-        low_qscore = min([rf1_qscore,rf2_qscore,if1_qscore,if2_qscore])
+    # if contains unknown base in index, write to low qual
+    if 'N' in if1_record[1] or 'N' in if2_record[1]:
+        for component in rf1_record:
+            lowqual1.write(component + '\n')
+        for component in rf2_record:
+            lowqual2.write(component + '\n')
+        unknown_counter += 1
+        continue
 
-        # check for N, unknown barcode, or low quality score and write to low_qual if so
-        # TODO: move the N check and missing check up above the quality question
-        if low_qscore < 20:
-            for component in rf1_record:
-                lowqual1.write(component + '\n')
-            for component in rf2_record:
-                lowqual2.write(component + '\n')
+    # if not valid barcode, write to low qual
+    if if1_record[1] not in barcodes or i2_rev not in barcodes:
+        for component in rf1_record:
+            lowqual1.write(component + '\n')
+        for component in rf2_record:
+            lowqual2.write(component + '\n')
+        lowqual_counter += 1
+        continue
 
-        # check for index match and write to match files for corresponding index
-        elif if1_record[1] == i2_rev:
-            for component in rf1_record:
-                barcodes[if1_record[1]]['read1file'].write(component + '\n')
-            for component in rf2_record:
-                barcodes[if1_record[1]]['read2file'].write(component + '\n') # added reference to second read file (added post 8/2 run)
-            barcodes[if1_record[1]]['counter'] += 1
+    # check quality score reads
+    # TODO: may be a good idea to work a quality calculation check in here (unittest)
+    # TODO: turn this into a function
+    # TODO: calculate quality scores inline so that we aren't wasting resources calculating all 4 every time (MAYBE THIS IS WHERE I'M LOSING TIME)
+    if1_qscore = sum([int(convert_phred(letter)) for letter in if1_record[3]]) / len(if1_record[3])
+    if2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
 
-        # otherwise write to mismatch file
-        else:
-            for component in rf1_record:
-                mismatch1.write(component + '\n')
-            for component in rf2_record:
-                mismatch2.write(component + '\n')
-            
-            # increment swap counter
-            swap_counter += 1
+    # rf1_qscore = sum([int(convert_phred(letter)) for letter in rf1_record[3]]) / len(rf1_record[3])
+    # rf2_qscore = sum([int(convert_phred(letter)) for letter in rf2_record[3]]) / len(rf2_record[3])
+
+    # lowest qscore
+    low_qscore = min([if1_qscore,if2_qscore])
+
+    # check for N, unknown barcode, or low quality score and write to low_qual if so
+    # TODO: move the N check and missing check up above the quality question
+    if low_qscore < 20:
+        for component in rf1_record:
+            lowqual1.write(component + '\n')
+        for component in rf2_record:
+            lowqual2.write(component + '\n')
+
+    # check for index match and write to match files for corresponding index
+    elif if1_record[1] == i2_rev:
+        for component in rf1_record:
+            barcodes[if1_record[1]]['read1file'].write(component + '\n')
+        for component in rf2_record:
+            barcodes[if1_record[1]]['read2file'].write(component + '\n') # added reference to second read file (added post 8/2 run)
+        barcodes[if1_record[1]]['counter'] += 1
+
+    # otherwise write to mismatch file
+    else:
+        for component in rf1_record:
+            mismatch1.write(component + '\n')
+        for component in rf2_record:
+            mismatch2.write(component + '\n')
+        
+        # increment swap dict
+        swap_dict[index_combo] += 1
+        
+        # increment swap counter
+        swap_counter += 1
+
+# close gzip files
+rf1.close()
+rf2.close()
+if1.close()
+if2.close()
+
+# close standnard output files
+lowqual1.close()
+lowqualt2.close()
+mismatch1.close()
+mismatch2.close()
 
 # close each of our dynamic output files
 for f in barcodes:
@@ -206,7 +240,27 @@ with open('summary_stats.md','w') as s:
     for sb in sorted(sum_barcodes):
         s.write('{}: '.format(sb) + str(sum_barcodes[sb]) + "%" + "<br>\n")
     
-    # write total index swaps
+    # write percentage for each index swap
     s.write("\n")
-    s.write('## Total index swaps\n')
-    s.write(str(swap_counter))
+    s.write('## Percentage of reads with each potential index swap:\n')
+    for swap in swap_dict:
+        swap_perc = round(swap_dict[swap] / recordcount * 100, 2)
+        s.write(swap + ': ' + str(swap_perc) + '%' + '<br>')
+    
+    # write total percentage index swapping
+    swap_perc_total = round(swap_counter / recordcount * 100, 2)
+    s.write('\n')
+    s.write('## Percentage total index swaps:\n')
+    s.write('All index swaps: ' + str(swap_perc_total) + '%')
+
+    # write unknowns
+    unknown_perc_total = round(unknown_counter / recordcount * 100, 2)
+    s.write('\n')
+    s.write('## Percentage unknown index:\n')
+    s.write('Unknown index: ' + str(unknown_perc_total) + '%')
+
+    # write low quality
+    lowqual_perc_total = round(lowqual_counter / recordcount * 100, 2)
+    s.write('\n')
+    s.write('## Percentage low quality index:\n')
+    s.write('Low quality: ' + str(lowqual_perc_total) + '%')
